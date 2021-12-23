2d0d816a3d83b92e683cba8503b92ee385f605fd
andrew herculano <andrewherculano@hotmail.com>
2021-12-23 09:02:44 -0300

    exibindo pontuação dentro do popup

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const quizForm = document.querySelector('.quiz-form')
+const popupAnswers = document.querySelector('.popup-wrapper')
+const answerPopup = document.querySelector('#answer')
 
 const correctAnswers = ['A', 'B', 'B', 'A']
+let score = 0
 
 quizForm.addEventListener('submit', event => {
   event.preventDefault()
 
-  let score = 0
   const userAnswers = [
     quizForm.inputQuestion1.value,
     quizForm.inputQuestion2.value,
@@ -19,5 +21,20 @@ quizForm.addEventListener('submit', event => {
     }
   })
 
-  console.log(score)
+  answerPopup.textContent = `${score}%`
+  popupAnswers.style.display = 'block'
+
+  score = 0
+})
+
+popupAnswers.addEventListener('click', event => {
+  const classNameClickedElement = event.target.classList[0]
+  const classNamesToClose = ['popup-close', 'button-continue']
+
+  const shouldClosePopup = classNamesToClose
+    .some(className => className === classNameClickedElement)
+
+  if (shouldClosePopup) {
+    popupAnswers.style.display = 'none'
+  }
 })
